9dc9a8db114c05620d45a4563d37190c5e07e03f
WarperSan <78861227+WarperSan@users.noreply.github.com>
2023-05-25 22:52:45 -0400

    Initial commit

---
+++ src/main/java/net/warpersan/warpermod/WarperModClient.java
@@ -0,0 +1,10 @@
+package net.warpersan.warpermod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WarperModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
