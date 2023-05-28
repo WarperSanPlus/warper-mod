f89286a5faa7c5dceb50e3d44086f1a3d55796d3
WarperSan <78861227+WarperSan@users.noreply.github.com>
2023-05-27 21:36:55 -0400

    A

--- src/main/java/net/warpersan/warpermod/WarperMod.java
+++ src/main/java/net/warpersan/warpermod/WarperMod.java
@@ -2,6 +2,14 @@ package net.warpersan.warpermod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.warpersan.warpermod.item.ModItemGroup;
+import net.warpersan.warpermod.item.ModItems;
+import net.warpersan.warpermod.item.SpringGun;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +21,7 @@ public class WarperMod implements ModInitializer {
     // That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("warpermod");
 
+
     @Override
     public void onInitialize() {
         // This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -20,5 +29,8 @@ public class WarperMod implements ModInitializer {
         // Proceed with mild caution.
 
         LOGGER.info("Hello Fabric world!");
+        ModItemGroup.registerItemGroups();
+        ModItems.registerModItems();
+        Registry.register(Registries.ITEM, new Identifier("warpermod", "spring_gun"), ModItems.SPRING_GUN);
     }
 }
